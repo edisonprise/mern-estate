4e81b3a460979068022a32b67f60f48fd5cbb4c5
Edinson <edisonprise@hotmail.com>
2023-12-31 16:23:16 -0500

    create and add OAuth component

---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-87c3f.firebaseapp.com",
+  projectId: "mern-estate-87c3f",
+  storageBucket: "mern-estate-87c3f.appspot.com",
+  messagingSenderId: "934606094719",
+  appId: "1:934606094719:web:4e8115b44552964ed71a4a",
+  measurementId: "G-5J6E2QMPWP"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js';
+import OAuth from '../components/OAuth.jsx';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -19,8 +20,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      
-      signInStart();
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',
       {
         method: 'POST',
@@ -51,6 +51,7 @@ export default function SignIn() {
         id='password' onChange={handleChange}/>
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg
         uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
+        <OAuth />
       </form>
       <div className='flex-gap-2 mt-5 onChange={handleChange}'>
         <p>Dont have an account?</p>
