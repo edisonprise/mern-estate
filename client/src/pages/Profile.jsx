febe35c29c1f5bacbd3ae8e77875b6602b0e4a0d
Edinson <edisonprise@hotmail.com>
2024-01-12 13:56:30 -0500

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,10 +15,9 @@ import {
   deleteUserStart,
   deleteUserSuccess,
   signOutUserStart,
- 
-  
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import { Link } from 'react-router-dom';
 
 
 export default function Profile() {
@@ -179,6 +178,10 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className='bg-green-700 text-white p-3 
+        rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser}className="text-red-700 cursor-pointer">
