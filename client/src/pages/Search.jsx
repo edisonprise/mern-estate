d4dde4ea5611c169094f3b0414cdd2c4dc8195e0
Edinson <edisonprise@hotmail.com>
2024-01-27 13:28:24 -0500

    fixing and saving

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -128,7 +128,7 @@ export default function Search() {
         <form onSubmit={handleSubmit} className="flex flex-col gap-8">
           <div className="flex items-center gap-2">
             <label className="whitespace-nowrap font-semibold">
-              Search Term:{" "}
+              Search Term:
             </label>
             <input
               type="text"
